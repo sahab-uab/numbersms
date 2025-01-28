8027d65b64bec32a3dbfd263cb8110d4766583f7
sahab-uab <sahabuabofficial@gmail.com>
2025-01-29 00:05:22 +0600

    Add not access page for public url

---
+++ backend/resources/views/errors/404.blade.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Page Not Access</title>
+    <style>
+        body {
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            padding: 0;
+            margin: 0;
+            height: 100vh;
+            width: 100%;
+        }
+        h1 {
+            font-size: 50px;
+        }
+        body {
+            font: 20px Helvetica, sans-serif;
+            color: #333;
+        }
+        article {
+            display: block;
+            text-align: left;
+            width: 650px;
+            margin: 0 auto;
+            padding: 20px;
+        }
+        @media only screen and (max-width: 600px){
+            article {
+                width: 100%;
+            }
+        }
+    </style>
+</head>
+<body>
+    <article>
+        <h1>Not Access</h1>
+        <div>
+            <p>Sorry, but you do not have permission to access this page.</p>
+        </div>
+    </article>
+</body>
+</html>
--- backend/routes/web.php
+++ backend/routes/web.php
@@ -2,6 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::fallback(function () {
+    return response()->view('errors.404', [], 404);
 });
