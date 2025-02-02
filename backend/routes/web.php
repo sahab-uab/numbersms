c820832c14a4e07cf51fd51917b6d9251465abcd
sahab-uab <sahabuabofficial@gmail.com>
2025-02-02 11:43:15 +0600

    Chnage web route file for ui login url

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -2,8 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 
+// ui login page
 Route::get('/login', function () {
-    return redirect()->to('/login');
+    return redirect()->to(env('UI_URL').'/login');
 })->name('login');
 
 Route::fallback(function () {
