d668725f96549b8e708e8154f18b48d6944e6f79
Abdullah Al Sayad <abdullahalsayad03@gmail.com>
2025-02-15 23:52:10 +0600

    issu fixed

--- frontend/src/Layout/AdminLayout.jsx
+++ frontend/src/Layout/AdminLayout.jsx
@@ -1,38 +1,9 @@
-import { useState, useEffect } from "react";
 import Sidebar from "../Components/AdminComponents/Sidebar";
 import { Outlet } from "react-router-dom";
-import TokenRefreshOnRouteChange from "../utils/TokenRefreshOnRouteChange";
+
 import ScrollToTop from "../utils/ScrollToTop";
-import { ClipLoader } from "react-spinners";
 
 const AdminLayout = () => {
-  const [isTokenRefreshed, setIsTokenRefreshed] = useState(false);
-
-  useEffect(() => {
-    const refreshToken = async () => {
-      try {
-        await TokenRefreshOnRouteChange();
-        setIsTokenRefreshed(true);
-      } catch (error) {
-        console.error("Token refresh failed:", error);
-        setIsTokenRefreshed(true);
-      }
-    };
-
-    refreshToken();
-  }, []);
-
-  if (!isTokenRefreshed) {
-    return (
-      <div className="flex items-center justify-center h-screen bg-gray-100">
-        <div className="flex flex-col items-center">
-          <ClipLoader size={50} color="#4F46E5" loading={true} />
-          <p className="mt-4 text-lg font-semibold text-gray-700">Loading...</p>
-        </div>
-      </div>
-    );
-  }
-
   return (
     <div className="flex min-h-screen">
       {/* Sidebar */}
